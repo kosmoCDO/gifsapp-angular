6b566dda11ebca8673bd9670502456a918e5280b
kosmo CDO <carlosdelassalas.kosmetikon@gmail.com>
2023-06-27 14:14:34 -0500

    gifs v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GifsService } from './gifs/services/gifs.service';
+import { Gif } from './gifs/interfaces/gifs.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gifs-app';
+
+  // constructor( private gifsService: GifsService ) {}
+
+
+
 }
